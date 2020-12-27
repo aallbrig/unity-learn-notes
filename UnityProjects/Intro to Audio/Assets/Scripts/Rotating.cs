2cb0ff02a0c993a77b8cb0357b87156914c2093e
Andrew Allbright <aallbrig@gmail.com>
2020-12-27 09:19:37 -0500

    Explore Unity - Foundations of real-time 3D sound complete

---
+++ UnityProjects/Intro to Audio/Assets/Scripts/Rotating.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+    public Vector3 rotateBy;
+
+    private void Update()
+    {
+        transform.Rotate(rotateBy);
+    }
+}
