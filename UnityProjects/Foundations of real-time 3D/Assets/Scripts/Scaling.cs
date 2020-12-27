465d65ccaf9d81f8d65f985eee2eb33544b5eded
Andrew Allbright <aallbrig@gmail.com>
2020-12-27 08:07:15 -0500

    Change a game object (properties) with a script

---
+++ UnityProjects/Foundations of real-time 3D/Assets/Scripts/Scaling.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Scaling : MonoBehaviour
+{
+    [Header("Scale")]
+    public Vector3 scaleBy;
+    public Vector3 scaleLimit = new Vector3(10, 10, 10);
+    [Header("Rotation")]
+    public Vector3 rotateBy;
+
+    private void Update()
+    {
+        if (transform.localScale.sqrMagnitude <= scaleLimit.sqrMagnitude)
+        {
+            transform.localScale += scaleBy;
+        }
+        transform.Rotate(rotateBy);
+    }
+}
