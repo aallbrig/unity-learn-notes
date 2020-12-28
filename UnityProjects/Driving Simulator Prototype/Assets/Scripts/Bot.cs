1a9d2a97e2b4f12dc47dc16919f56db41d58244d
Andrew Allbright <aallbrig@gmail.com>
2020-12-28 13:25:19 -0500

    More bots with simpler customize script

--- UnityProjects/Driving Simulator Prototype/Assets/Scripts/Bot.cs
+++ UnityProjects/Driving Simulator Prototype/Assets/Scripts/Bot.cs
@@ -1,31 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Bot : MonoBehaviour
 {
     [Header("Y-up vector (in meters per second)")]
     public Vector3 vector = new Vector3(0, 0, 10);
+
+    [System.Serializable]
+    public struct ActivateModel
+    {
+        public GameObject target;
+        public bool active;
+    }
     
-    [Header("Enable model (unique weight in kilograms)")]
-    public GameObject armoredCar;
-    public bool armoredCarActive = false;
-    public GameObject bus;
-    public bool busActive = false;
-    public GameObject blueCar;
-    public bool blueCarActive = false;
-    public GameObject redCar;
-    public bool redCarActive = true;
-    public GameObject greenVan;
-    public bool greenVanActive = false;
+    public List<ActivateModel> modelsToActivate;
 
     private void Start()
     {
-        armoredCar.SetActive(armoredCarActive);
-        bus.SetActive(busActive);
-        blueCar.SetActive(blueCarActive);
-        redCar.SetActive(redCarActive);
-        greenVan.SetActive(greenVanActive);
+        modelsToActivate.ForEach((model) => model.target.SetActive(model.active));
     }
 
     private void Update()
