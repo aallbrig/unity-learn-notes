3b6132cd113a0875d334e8e5be8e3f10c2dc1bc0
Andrew Allbright <aallbrig@gmail.com>
2020-12-28 12:50:40 -0500

    Players can "race" against a configurable bot

---
+++ UnityProjects/Driving Simulator Prototype/Assets/Scripts/Bot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour
+{
+    [Header("Y-up vector (in meters per second)")]
+    public Vector3 vector = new Vector3(0, 0, 10);
+    
+    [Header("Enable model (unique weight in kilograms)")]
+    public GameObject armoredCar;
+    public bool armoredCarActive = false;
+    public GameObject bus;
+    public bool busActive = false;
+    public GameObject blueCar;
+    public bool blueCarActive = false;
+    public GameObject redCar;
+    public bool redCarActive = true;
+    public GameObject greenVan;
+    public bool greenVanActive = false;
+
+    private void Start()
+    {
+        armoredCar.SetActive(armoredCarActive);
+        bus.SetActive(busActive);
+        blueCar.SetActive(blueCarActive);
+        redCar.SetActive(redCarActive);
+        greenVan.SetActive(greenVanActive);
+    }
+
+    private void Update()
+    {
+        transform.Translate( vector * Time.deltaTime);
+    }
+}
---
+++ UnityProjects/Driving Simulator Prototype/Assets/Scripts/VehicleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour
+{
+    [Header("Speed in meters per second")]
+    public float speed = 10;
+
+    public float turnSpeed = 10;
+
+    private void Update()
+    {
+        transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Input.GetAxis("Horizontal") * Vector3.up * Time.deltaTime * turnSpeed);
+    }
+}
