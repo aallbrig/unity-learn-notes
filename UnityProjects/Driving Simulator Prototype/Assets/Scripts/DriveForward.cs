d1c5395019221944570cf3605bf0a22a0c419c85
Andrew Allbright <aallbrig@gmail.com>
2020-12-28 09:39:29 -0500

    Longer road complete with caution cones and a driving player

---
+++ UnityProjects/Driving Simulator Prototype/Assets/Scripts/DriveForward.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveForward : MonoBehaviour
+{
+    public Vector3 vector = new Vector3(0, 0, 20);
+
+    private void Update()
+    {
+        transform.localPosition += vector * Time.deltaTime;
+    }
+}
