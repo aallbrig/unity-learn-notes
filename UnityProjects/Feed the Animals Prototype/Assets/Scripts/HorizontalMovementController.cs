00afb8029b61c2783d1a5e5c67b92af02f2f6218
Andrew Allbright <aallbrig@gmail.com>
2020-12-29 14:49:31 -0500

    Prototype 2 finish

--- UnityProjects/Feed the Animals Prototype/Assets/Scripts/HorizontalMovementController.cs
+++ UnityProjects/Feed the Animals Prototype/Assets/Scripts/HorizontalMovementController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.WSA;
+﻿using UnityEngine;
 
 public class HorizontalMovementController : MonoBehaviour
 {
---
+++ UnityProjects/Feed the Animals Prototype/Assets/Scripts/InvertNormals.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class InvertNormals : MonoBehaviour
+{
+    private void Start()
+    {
+        var mesh = GetComponent<MeshFilter>().mesh;
+        var normals = mesh.normals;
+        for (var i = 0; i < normals.Length; i++)
+            normals[i] = -1 * normals[i];
+
+        mesh.normals = normals;
+
+        for (var i = 0; i < mesh.subMeshCount; i++)
+        {
+            var tris = mesh.GetTriangles(i);
+            for (var j = 0; j < tris.Length; j += 3)
+            {
+                var temp = tris[j];
+                tris[j] = tris[j + 1];
+                tris[j + 1] = temp;
+            }
+            mesh.SetTriangles(tris, i);
+        }
+    }
+}
