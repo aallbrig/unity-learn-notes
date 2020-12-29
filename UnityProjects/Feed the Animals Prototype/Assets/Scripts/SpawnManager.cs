c40315f17f24ce218d944d57d0dbbcc5f380b56d
Andrew Allbright <aallbrig@gmail.com>
2020-12-29 13:58:43 -0500

    Animals spawn at timed intervals

--- UnityProjects/Feed the Animals Prototype/Assets/Scripts/SpawnManager.cs
+++ UnityProjects/Feed the Animals Prototype/Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,36 @@ public class SpawnManager : MonoBehaviour
     public bool spawnKeyEnabled;
     public GameObject animal;
     public float spawnRangeX = 20;
+    [Header("Spawn interval time (in seconds)")]
+    public float spawnInterval = 1.5f;
+    public bool spawnTimeEnabled = true;
+
+    private float _timeSinceSpawn;
+    
+    private void SpawnAnimal()
+    {
+        Instantiate(
+            animal,
+            transform.position + new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, 0),
+            transform.rotation
+        );
+    }
+
+    private void Start()
+    {
+        _timeSinceSpawn = Time.time;
+    }
 
     private void Update()
     {
-        if (spawnKeyEnabled && Input.GetKeyDown(spawnKey))
+        var spawnFromKey = spawnKeyEnabled && Input.GetKeyDown(spawnKey);
+        var spawnFromTimer = spawnTimeEnabled && (Time.time - _timeSinceSpawn) > spawnInterval;
+        
+        if (spawnFromKey) SpawnAnimal();
+        if (spawnFromTimer)
         {
-            Instantiate(
-                animal,
-                transform.position + new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, 0),
-                transform.rotation
-            );
+            SpawnAnimal();
+            _timeSinceSpawn = Time.time;
         }
     }
 }
