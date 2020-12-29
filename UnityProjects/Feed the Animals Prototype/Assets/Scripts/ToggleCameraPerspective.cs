c3c5813d3c4885be20afc2dad193b8045b84c627
Andrew Allbright <aallbrig@gmail.com>
2020-12-29 13:44:42 -0500

    Camera can be toggled between orthographic by pressing E

---
+++ UnityProjects/Feed the Animals Prototype/Assets/Scripts/ToggleCameraPerspective.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ToggleCameraPerspective : MonoBehaviour
+{
+    public KeyCode toggleKey = KeyCode.E;
+    public bool toggleKeyEnabled;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (toggleKeyEnabled && Input.GetKeyDown(toggleKey))
+        {
+            _camera.orthographic = !_camera.orthographic;
+        }
+    }
+}
