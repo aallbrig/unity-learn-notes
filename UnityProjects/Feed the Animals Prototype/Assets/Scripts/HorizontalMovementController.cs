51a286aa26b805240ca310fc75831ff6dacef476
Andrew Allbright <aallbrig@gmail.com>
2020-12-29 10:22:48 -0500

    Player can move horizontally and vertically within bounds

---
+++ UnityProjects/Feed the Animals Prototype/Assets/Scripts/HorizontalMovementController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class HorizontalMovementController : MonoBehaviour
+{
+    [Header("Speed (in meters per second)")]
+    public float speed = 35;
+    public float xBounds = 22;
+    public float zBounds = 10;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        
+        var pos = transform.position;
+        if (pos.x < -xBounds)
+        {
+            transform.position = new Vector3(-xBounds, pos.y, pos.z);
+        } else if (pos.x > xBounds)
+        {
+            transform.position = new Vector3(xBounds, pos.y, pos.z);
+        }
+
+        if (pos.z < -zBounds)
+        {
+            transform.position = new Vector3(transform.position.x, pos.y, -zBounds);
+        } else if (pos.z > zBounds)
+        {
+            transform.position = new Vector3(transform.position.x, pos.y, zBounds);
+        }
+    }
+}
---
+++ UnityProjects/Feed the Animals Prototype/Assets/Scripts/LookAtTransform.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class LookAtTransform : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 rotateOffset;
+    private bool _isTargetNull;
+
+    private void Start()
+    {
+        _isTargetNull = target == null;
+    }
+
+    private void Update()
+    {
+        if (_isTargetNull) return;
+
+        transform.LookAt(target);
+        transform.Rotate(rotateOffset);
+    }
+}
