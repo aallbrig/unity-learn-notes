527aaf854093a22faccf5c77622692a3e85ba021
Andrew Allbright <aallbrig@gmail.com>
2020-12-29 16:33:18 -0500

    Challenge 2 complete

---
+++ UnityProjects/Play Fetch Challenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    [Header("Fire interval (in seconds)")]
+    public float fireInterval = 0.5f;
+    private float _timeSinceLastFire;
+
+    void Start()
+    {
+        _timeSinceLastFire = Time.time - fireInterval;
+    }
+    void Update()
+    {
+        var canFire = Time.time - _timeSinceLastFire > fireInterval;
+        if (canFire && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            _timeSinceLastFire = Time.time;
+        }
+    }
+}
