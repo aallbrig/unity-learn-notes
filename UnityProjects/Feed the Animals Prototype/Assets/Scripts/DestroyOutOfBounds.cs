ddf8212ac95a05c0ddbfb9854cd8ea80f1872a48
Andrew Allbright <aallbrig@gmail.com>
2020-12-29 13:39:06 -0500

    Animals spawn in a randomized X location

---
+++ UnityProjects/Feed the Animals Prototype/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBounds = 70;
+    public float bottomBounds = -10;
+
+    private void Update()
+    {
+        if (transform.position.z > topBounds || transform.position.z < bottomBounds) Destroy(gameObject);
+    }
+}
---
+++ UnityProjects/Feed the Animals Prototype/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public KeyCode spawnKey = KeyCode.S;
+    public bool spawnKeyEnabled;
+    public GameObject animal;
+    public float spawnRangeX = 20;
+
+    private void Update()
+    {
+        if (spawnKeyEnabled && Input.GetKeyDown(spawnKey))
+        {
+            Instantiate(
+                animal,
+                transform.position + new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, 0),
+                transform.rotation
+            );
+        }
+    }
+}
