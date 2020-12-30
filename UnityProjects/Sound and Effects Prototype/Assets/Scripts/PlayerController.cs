22f018d982edba08b0bf0b9f1f52cb223bb8a347
Andrew Allbright <aallbrig@gmail.com>
2020-12-30 13:43:39 -0500

    Gravity modifier

--- UnityProjects/Sound and Effects Prototype/Assets/Scripts/PlayerController.cs
+++ UnityProjects/Sound and Effects Prototype/Assets/Scripts/PlayerController.cs
@@ -3,15 +3,17 @@
 public class PlayerController : MonoBehaviour
 {
     public KeyCode jumpKey = KeyCode.Space;
-    public float jumpForce = 100;
+    public float jumpForce = 10;
+    public float gravityModifier = 10;
     private Rigidbody _rigidbody;
 
     private void Jump()
     {
-        _rigidbody.AddForce(Vector3.up * jumpForce);
+        _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
     private void Start()
     {
+        Physics.gravity *= gravityModifier;
         _rigidbody = GetComponent<Rigidbody>();
         Jump();
     }
