c318001282ddef477299e16e5bc913a49a666b90
Andrew Allbright <aallbrig@gmail.com>
2020-12-30 13:38:47 -0500

    Player can jump on space

---
+++ UnityProjects/Sound and Effects Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public KeyCode jumpKey = KeyCode.Space;
+    public float jumpForce = 100;
+    private Rigidbody _rigidbody;
+
+    private void Jump()
+    {
+        _rigidbody.AddForce(Vector3.up * jumpForce);
+    }
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        Jump();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            Jump();
+        }
+    }
+}
