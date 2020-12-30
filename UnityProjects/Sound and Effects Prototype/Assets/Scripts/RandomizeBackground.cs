5b154086cce8b3d1e5e6febb805f377439ea13f0
Andrew Allbright <aallbrig@gmail.com>
2020-12-30 13:20:19 -0500

    Prototype 3 start - sounds, effects, and music

---
+++ UnityProjects/Sound and Effects Prototype/Assets/Scripts/RandomizeBackground.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeBackground : MonoBehaviour
+{
+    public List<Sprite> listOfPossibleSprites;
+    private SpriteRenderer _spriteRender;
+
+    private void Start()
+    {
+        listOfPossibleSprites = listOfPossibleSprites ?? new List<Sprite>();
+        _spriteRender = GetComponent<SpriteRenderer>();
+        _spriteRender.sprite = listOfPossibleSprites[Random.Range(0, listOfPossibleSprites.Count - 1)];
+    }
+}
