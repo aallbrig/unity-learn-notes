d8acb7eb28e5c8c66c0c11435edc06e4f6750100
Andrew Allbright <aallbrig@gmail.com>
2020-12-30 13:31:17 -0500

    Player controls a randomized farm or town character

---
+++ UnityProjects/Sound and Effects Prototype/Assets/Scripts/RandomizePrefab.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomizePrefab : MonoBehaviour
+{
+    [Header("Location of prefabs")] public List<GameObject> targetGameObjects;
+
+    private readonly List<GameObject> _listOfPrefabs = new List<GameObject>();
+    
+    private void SetActiveIndex(int index)
+    {
+    }
+    private void Start()
+    {
+        if (targetGameObjects == null) return;
+
+        foreach (GameObject targetGameObject in targetGameObjects)
+        {
+            foreach (Transform child in targetGameObject.transform)
+            {
+                _listOfPrefabs.Add(child.gameObject);
+            }
+        }
+
+        var index = Random.Range(0, _listOfPrefabs.Count - 1);
+
+        for (var i = 0; i < _listOfPrefabs.Count; i++)
+        {
+            _listOfPrefabs[i].SetActive(i == index);
+        }
+    }
+}
