fc87f45f63028b0b70696d8d870aeae04a9e213c
Andrew Allbright <aallbrig@gmail.com>
2021-01-02 10:48:26 -0500

    Player can rotate the camera and move forward

---
+++ UnityProjects/Gameplay Mechanics Prototype/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(
+            Vector3.up,
+            Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime
+        );
+    }
+}
---
+++ UnityProjects/Gameplay Mechanics Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor.Experimental.UIElements;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _rigidbody.AddForce(Input.GetAxis("Vertical") * Vector3.forward * speed);
+    }
+}
