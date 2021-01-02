afe03ddcb813df01840d70c7861497dd2423c5d7
Andrew Allbright <aallbrig@gmail.com>
2021-01-01 20:47:45 -0500

    In game bombs and money icons are spawned by the spawn manager

--- UnityProjects/Balloons and Bombs Challenge/Assets/Challenge 3/Scripts/SpawnManagerX.cs
+++ UnityProjects/Balloons and Bombs Challenge/Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -1,34 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] objectPrefabs;
-    private float spawnDelay = 2;
-    private float spawnInterval = 1.5f;
+    private const float SpawnDelay = 2;
+    private const float SpawnInterval = 1.5f;
+    private PlayerControllerX _playerControllerScript;
 
-    private PlayerControllerX playerControllerScript;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("PrawnsObject", spawnDelay, spawnInterval);
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
-    }
-
-    // Spawn obstacles
-    void SpawnObjects ()
+    private void SpawnObjects ()
     {
         // Set random spawn location and random object index
-        Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
-        int index = Random.Range(0, objectPrefabs.Length);
+        var spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
+        var randomIndex = Random.Range(0, objectPrefabs.Length);
 
         // If game is still active, spawn new object
-        if (!playerControllerScript.gameOver)
+        if (!_playerControllerScript.gameOver)
         {
-            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+            Instantiate(objectPrefabs[randomIndex], spawnLocation, objectPrefabs[randomIndex].transform.rotation);
         }
 
     }
+    private void Start()
+    {
+        _playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+        InvokeRepeating("SpawnObjects", SpawnDelay, SpawnInterval);
+    }
+
 }
