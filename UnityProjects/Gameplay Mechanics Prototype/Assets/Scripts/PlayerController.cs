dde7a1657269ecab9b76a457b712d71541a5beb4
Andrew Allbright <aallbrig@gmail.com>
2021-01-02 11:44:27 -0500

    Power up gives player ability to pulse enemy away

--- UnityProjects/Gameplay Mechanics Prototype/Assets/Scripts/PlayerController.cs
+++ UnityProjects/Gameplay Mechanics Prototype/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,20 @@
-﻿using UnityEditor.Experimental.UIElements;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5;
+    public bool hasPowerup = false;
+    private float _powerupStrength = 15;
     private Rigidbody _rigidbody;
     private GameObject _focalPoint;
 
+    private IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+    }
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -16,5 +24,26 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         _rigidbody.AddForce(Input.GetAxis("Vertical") * _focalPoint.transform.forward * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (hasPowerup && other.gameObject.CompareTag("Enemy"))
+        {
+            var enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            var awayFromPlayer = (other.gameObject.transform.position - transform.position).normalized;
+
+            enemyRigidbody.AddForce(awayFromPlayer * _powerupStrength, ForceMode.Impulse);
+        }
     }
 }
