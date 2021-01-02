82bf3ab892212b8d68ab047ab28ff21ef47c04d5
Andrew Allbright <aallbrig@gmail.com>
2021-01-02 12:20:44 -0500

    Power ups rotate

---
+++ UnityProjects/Gameplay Mechanics Prototype/Assets/Scripts/Rotating.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+    [Header("Revolutions Per Second")]
+    public float revolutionsPerSecond;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, (360 * revolutionsPerSecond) * Time.deltaTime);
+    }
+}
