2ca30fefc3cae9a2e6ecd67ab6f362d24f253384
Andrew Allbright <aallbrig@gmail.com>
2021-01-02 10:51:15 -0500

    Player moves towards the focal point's forward

--- UnityProjects/Gameplay Mechanics Prototype/Assets/Scripts/PlayerController.cs
+++ UnityProjects/Gameplay Mechanics Prototype/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,16 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5;
     private Rigidbody _rigidbody;
+    private GameObject _focalPoint;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _focalPoint = GameObject.Find("Focal Point");
     }
 
     private void Update()
     {
-        _rigidbody.AddForce(Input.GetAxis("Vertical") * Vector3.forward * speed);
+        _rigidbody.AddForce(Input.GetAxis("Vertical") * _focalPoint.transform.forward * speed);
     }
 }
