d81496850807ac1626ed2b8f7342d5d355f3e459
Andrew Allbright <aallbrig@gmail.com>
2021-01-02 11:06:46 -0500

    Enemy follows the player and the physics collisions are bouncy

---
+++ UnityProjects/Gameplay Mechanics Prototype/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 5;
+    public GameObject player;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _rigidbody.AddForce(
+            (player.transform.position - transform.position).normalized * speed
+        );
+    }
+}
