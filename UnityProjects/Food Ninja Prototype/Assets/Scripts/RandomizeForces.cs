6c6dc59bc08373f67c227c4769d2538b0b0e9222
Andrew Allbright <aallbrig@gmail.com>
2021-01-03 14:03:09 -0500

    Good and bad prefabs start out with randomized force, torque, and position

---
+++ UnityProjects/Food Ninja Prototype/Assets/Scripts/RandomizeForces.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class RandomizeForces : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private float _forceLower = 12;
+    private float _forceUpper = 16;
+    private float _torqueLower = -10;
+    private float _torqueUpper = 10;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(Vector3.up * Random.Range(_forceLower, _forceUpper), ForceMode.Impulse);
+        _rigidbody.AddTorque(
+            Random.Range(_torqueLower, _forceUpper),
+            Random.Range(_torqueLower, _torqueUpper),
+            Random.Range(_torqueLower, _torqueUpper),
+            ForceMode.Impulse
+        );
+    }
+}
---
+++ UnityProjects/Food Ninja Prototype/Assets/Scripts/RandomizePosition.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RandomizePosition : MonoBehaviour
+{
+    private const float XRange = 4;
+    private const float YStart = -6;
+
+    private void Start()
+    {
+        transform.position = new Vector3(Random.Range(-XRange, XRange), YStart);
+    }
+}
