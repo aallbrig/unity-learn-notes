1121a51c388eefebd7ebb5c4dde4145b01a60526
Andrew Allbright <aallbrig@gmail.com>
2021-01-03 14:15:09 -0500

    Clicking on targets will destroy them

--- UnityProjects/Food Ninja Prototype/Assets/Scripts/Target.cs
+++ UnityProjects/Food Ninja Prototype/Assets/Scripts/Target.cs
@@ -1,6 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private void OnMouseDown()
+    {
+       Destroy(gameObject);
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
 }
