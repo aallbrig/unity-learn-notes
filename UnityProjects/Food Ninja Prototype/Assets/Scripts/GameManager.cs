9d1df99a349feffb18fa5a93d52490d397d21e50
Andrew Allbright <aallbrig@gmail.com>
2021-01-03 14:11:25 -0500

    Every second, either a good or bad target is spawned

---
+++ UnityProjects/Food Ninja Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("List of prefabs 'targets' to periodically spawn")]
+    public List<GameObject> targets;
+
+    [Header("Spawn rate in seconds")]
+    public float spawnRate = 1;
+
+    private IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            Instantiate(targets[Random.Range(0, targets.Count)]);
+        }
+    }
+    
+    private void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+}
--- UnityProjects/Food Ninja Prototype/Assets/Scripts/RandomizePosition.cs
+++ UnityProjects/Food Ninja Prototype/Assets/Scripts/RandomizePosition.cs
@@ -3,7 +3,7 @@
 public class RandomizePosition : MonoBehaviour
 {
     private const float XRange = 4;
-    private const float YStart = -6;
+    private const float YStart = -1;
 
     private void Start()
     {
