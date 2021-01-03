d468860129caaec290615714a3cdb10f415ef1da
Andrew Allbright <aallbrig@gmail.com>
2021-01-03 15:13:46 -0500

    Player can see that they gets +1 score for every target they destroy

--- UnityProjects/Food Ninja Prototype/Assets/Scripts/GameManager.cs
+++ UnityProjects/Food Ninja Prototype/Assets/Scripts/GameManager.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [Header("List of prefabs 'targets' to periodically spawn")]
-    public List<GameObject> targets;
+    [SerializeField]
+    private List<GameObject> targets;
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
 
     [Header("Spawn rate in seconds")]
-    public float spawnRate = 1;
+    [SerializeField]
+    private float spawnRate = 1;
+    private int _score;
+
+    public void UpdateScore(int score)
+    {
+        _score += score;
+    }
+
+    private void RenderScoreText()
+    {
+        scoreText.text = "Score: " + _score;
+    }
 
     private IEnumerator SpawnTarget()
     {
@@ -21,6 +37,13 @@ public class GameManager : MonoBehaviour
     
     private void Start()
     {
+        _score = 0;
+
         StartCoroutine(SpawnTarget());
+    }
+
+    private void Update()
+    {
+        RenderScoreText();
     }
 }
--- UnityProjects/Food Ninja Prototype/Assets/Scripts/Target.cs
+++ UnityProjects/Food Ninja Prototype/Assets/Scripts/Target.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private const int PointRewards = 1;
+    private GameManager _gameManager;
+
+    private void Start()
+    {
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
     private void OnMouseDown()
     {
        Destroy(gameObject);
+       _gameManager.UpdateScore(PointRewards);
     }
 
     private void OnTriggerEnter(Collider other)
