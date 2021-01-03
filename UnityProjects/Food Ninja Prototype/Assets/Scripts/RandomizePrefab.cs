d466bb8b17cb9188cc24bb0dbaa2c4e5f2c0c7f3
Andrew Allbright <aallbrig@gmail.com>
2021-01-03 13:52:38 -0500

    Good and bad (randomized) prefabs ready for further scripting

---
+++ UnityProjects/Food Ninja Prototype/Assets/Scripts/RandomizePrefab.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RandomizePrefab : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public GameObject activeGameObject;
+    private List<GameObject> _prefabs;
+
+    private static List<GameObject> PopulateListOfPrefabs(IEnumerable<GameObject> targets)
+    {
+        var allTargetTransforms = targets.SelectMany(targetGameObject => targetGameObject.transform.Cast<Transform>());
+        return allTargetTransforms.Select(child => child.gameObject).ToList();
+    }
+
+    private void Start()
+    {
+        _prefabs = PopulateListOfPrefabs(targets);
+        var randomIndex = Random.Range(0, _prefabs.Count);
+
+        for (var i = 0; i < _prefabs.Count; i++)
+        {
+            var prefab = _prefabs[i];
+            prefab.gameObject.SetActive(i == randomIndex);
+
+            if (i == randomIndex)
+                activeGameObject = prefab;
+        }
+    }
+}
