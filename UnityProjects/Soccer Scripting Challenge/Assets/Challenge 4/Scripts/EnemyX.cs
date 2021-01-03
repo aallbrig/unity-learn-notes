ae0b156031a484164906a461fc39a21f96f090ff
Andrew Allbright <aallbrig@gmail.com>
2021-01-03 01:09:03 -0500

    The wave has n enemies, where n is the wave number

--- UnityProjects/Soccer Scripting Challenge/Assets/Challenge 4/Scripts/EnemyX.cs
+++ UnityProjects/Soccer Scripting Challenge/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -1,41 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyX : MonoBehaviour
 {
     public float speed;
-    private Rigidbody enemyRb;
-    private GameObject playerGoal;
+    private Rigidbody _enemyRigidbody;
+    private GameObject _playerGoal;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        enemyRb = GetComponent<Rigidbody>();
-        playerGoal = GameObject.Find("Player Goal");
+        _enemyRigidbody = GetComponent<Rigidbody>();
+        _playerGoal = GameObject.Find("Player Goal");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // Set enemy direction towards player goal and move there
-        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
-        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+        var lookDirection = (_playerGoal.transform.position - transform.position).normalized;
+        _enemyRigidbody.AddForce(lookDirection * speed * Time.deltaTime);
 
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        // If enemy collides with either goal, destroy it
-        if (other.gameObject.name == "Enemy Goal")
+        switch (other.gameObject.name)
         {
-            Destroy(gameObject);
-        } 
-        else if (other.gameObject.name == "Player Goal")
-        {
-            Destroy(gameObject);
+            // If enemy collides with either goal, destroy it
+            case "Enemy Goal":
+            case "Player Goal":
+                Destroy(gameObject);
+                break;
         }
-
     }
-
 }
