7da62e5e03419f1505f61cec1cdc5877c0d7bae1
Andrew Allbright <aallbrig@gmail.com>
2021-01-11 04:19:39 -0500

    User can change the color of the cubes (thanks to command pattern)

---
+++ UnityProjects/The Command Pattern Prototype/Assets/Scripts/UserClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UserClick : MonoBehaviour
+{
+    private Camera _camera;
+    private List<ICommand> actionQueue = new List<ICommand>();
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit))
+            {
+                if (hit.collider.gameObject.CompareTag("Cube"))
+                {
+                    var cube = hit.collider.gameObject;
+                    var randomColor = new Color(Random.value, Random.value, Random.value);
+                    var click = new ClickCommand(cube, randomColor);
+                    actionQueue.Add(click);
+                    click.Execute();
+                }
+            }
+        }
+    }
+}
---
+++ UnityProjects/The Command Pattern Prototype/Assets/Scripts/interfaces/ClickCommand.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ClickCommand : ICommand
+{
+    private readonly GameObject _cube;
+    private readonly Color _color;
+    private readonly MeshRenderer _meshRenderer;
+    private Color _previousColor;
+
+    public ClickCommand(GameObject cube, Color color)
+    {
+        _cube = cube;
+        _color = color;
+        _meshRenderer = _cube.GetComponent<MeshRenderer>();
+    }
+
+    public void Execute()
+    {
+        _previousColor = _meshRenderer.material.color;
+        _meshRenderer.material.color = _color;
+    }
+
+    public void Undo()
+    {
+        _meshRenderer.material.color = _previousColor;
+    }
+}
