d0cb8c3ea2ad25550cab5f5d3a5f8609dd27cdd4
Andrew Allbright <aallbrig@gmail.com>
2021-01-12 21:08:56 -0500

    UI Fundamentals practice exercise

---
+++ UnityProjects/User Interface Fundamentals/Assets/Scripts/OnButtonActive.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OnButtonActive : MonoBehaviour
+{
+    public Image cursorImage;
+
+    public void ShowCursor() => cursorImage.gameObject.SetActive(true);
+
+    public void HideCursor() => cursorImage.gameObject.SetActive(false);
+}
---
+++ UnityProjects/User Interface Fundamentals/Assets/Scripts/OpenPanel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OpenPanel : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject panelOpenFocusElement;
+
+    private GameObject _gameObject;
+
+    public void OnPanelOpen()
+    {
+        panel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(panelOpenFocusElement);
+    }
+
+    public void OnPanelClose()
+    {
+        panel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(_gameObject);
+    }
+
+    private void Start()
+    {
+        _gameObject = gameObject;
+    }
+
+    private void Update()
+    {
+        if (!panel.active) return;
+
+        if (Input.GetKeyDown(KeyCode.Escape)) OnPanelClose();
+    }
+}
