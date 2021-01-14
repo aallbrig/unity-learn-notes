c6ea7348638dfa79fd6a000fd267d76b92f47053
Andrew Allbright <aallbrig@gmail.com>
2021-01-13 21:07:29 -0500

    Creating a character inventory system project start

---
+++ UnityProjects/Creating a Character Inventory System/Assets/3. GameManager/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Button ResumeButton;
+    public Button QuitButton;
+    public Button RestartButton;
+
+    private void Awake()
+    {
+        ResumeButton.onClick.AddListener(HandleResumeClick);
+        QuitButton.onClick.AddListener(HandleQuitClick);
+        RestartButton.onClick.AddListener(HandleRestartClick);
+    }
+
+    private void HandleResumeClick()
+    {
+        GameManager.Instance.TogglePause();
+    }
+
+    private void HandleQuitClick()
+    {
+        GameManager.Instance.QuitGame();
+    }
+
+    private void HandleRestartClick()
+    {
+        GameManager.Instance.RestartGame();
+    }
+}
