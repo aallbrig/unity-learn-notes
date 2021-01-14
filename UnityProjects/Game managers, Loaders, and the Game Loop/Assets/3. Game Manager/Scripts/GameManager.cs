d5eb229e1ed91bd84992315398ba236eb88fc512
Andrew Allbright <aallbrig@gmail.com>
2021-01-14 15:12:27 -0500

    Stub out files and configure settings for project

---
+++ UnityProjects/Game managers, Loaders, and the Game Loop/Assets/3. Game Manager/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Keep track of the current level
+    
+    // Load level
+    
+    // Unload level
+    
+    // Keep track of game state
+    
+    // Generate other persistent systems
+}
