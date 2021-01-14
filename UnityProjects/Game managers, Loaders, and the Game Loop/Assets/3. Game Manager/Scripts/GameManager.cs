9c1f6b82d586d95171988f6a2bf9806f82093c45
Andrew Allbright <aallbrig@gmail.com>
2021-01-14 15:42:20 -0500

    GameManager implements Singleton pattern

--- UnityProjects/Game managers, Loaders, and the Game Loop/Assets/3. Game Manager/Scripts/GameManager.cs
+++ UnityProjects/Game managers, Loaders, and the Game Loop/Assets/3. Game Manager/Scripts/GameManager.cs
@@ -1,14 +1,65 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     // Keep track of the current level
+    private string _currentLevelName = string.Empty;
+    private List<AsyncOperation> _loadOperations = new List<AsyncOperation>();
     
     // Load level
+    public void LoadLevel(string levelName)
+    {
+        var asyncOp = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+        if (asyncOp == null)
+        {
+           Debug.LogError("[GameManager] Unable to load level " + levelName);
+           return;
+        }
+        
+        _loadOperations.Add(asyncOp);
+
+        asyncOp.completed += OnLoadOperationComplete;
+        _currentLevelName = levelName;
+    }
     
     // Unload level
-    
+    public void UnloadLevel(string levelName)
+    {
+        var asyncOp = SceneManager.UnloadSceneAsync(levelName);
+        if (asyncOp == null)
+        {
+           Debug.LogError("[GameManager] Unable to unload level " + levelName);
+           return;
+        }
+
+        _loadOperations.Add(asyncOp);
+
+        asyncOp.completed += OnUnloadOperationComplete;
+        _currentLevelName = string.Empty;
+    }
+     
     // Keep track of game state
     
     // Generate other persistent systems
+
+    private void OnLoadOperationComplete(AsyncOperation asyncOp)
+    {
+        if (_loadOperations.Contains(asyncOp)) _loadOperations.Remove(asyncOp);
+        Debug.Log("Load Complete");
+    }
+    
+    private void OnUnloadOperationComplete(AsyncOperation asyncOp)
+    {
+        if (_loadOperations.Contains(asyncOp)) _loadOperations.Remove(asyncOp);
+        Debug.Log("Unload Complete");
+    }
+
+    private void Start()
+    {
+        // This game object should never be destroyed
+        DontDestroyOnLoad(gameObject);
+        LoadLevel("Main");
+    }
 }
