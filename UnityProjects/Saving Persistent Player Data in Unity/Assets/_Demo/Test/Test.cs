6e78aa324348750ea2eaf1617f2d693a407f256a
Andrew Allbright <aallbrig@gmail.com>
2021-01-14 07:55:43 -0500

    Demonstrate serializable fields in Unity scripts

---
+++ UnityProjects/Saving Persistent Player Data in Unity/Assets/_Demo/Test/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int i;
+    public float f;
+    public string s;
+    public Vector3 v;
+    public Color c;
+    public LayerMask l;
+    public AnimationCurve a;
+
+    public GameObject g;
+    public Rigidbody r;
+    public Transform t;
+
+    public int[] ints;
+    public float[] floats;
+
+    public C cl;
+
+    public S str;
+
+    [Serializable]
+    public class C
+    {
+        public int i;
+        public float f;
+    }
+
+    [Serializable]
+    public struct S
+    {
+        public int i;
+        public float f;
+    }
+}
