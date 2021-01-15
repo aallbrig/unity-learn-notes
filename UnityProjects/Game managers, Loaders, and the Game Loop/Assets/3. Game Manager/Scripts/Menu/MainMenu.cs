398f9246ab37ad491c22543e37f096bd1a011528
Andrew Allbright <aallbrig@gmail.com>
2021-01-15 06:28:21 -0500

    Boot scene transitions to main game when the player presses space

---
+++ UnityProjects/Game managers, Loaders, and the Game Loop/Assets/3. Game Manager/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Functions to play fade in/out animations
+    [SerializeField] private Animation _mainMenuAnimator;
+
+    #region FadeIn
+    [SerializeField] private AnimationClip _fadeInAnimation;
+
+    public void PlayFadeIn() => PlayClip(_fadeInAnimation);
+
+    public void OnFadeInStart()
+    {
+        Debug.LogWarning("On fade in start");
+    }
+
+    public void OnFadeInComplete()
+    {
+        Debug.LogWarning("On fade in complete");
+        UIManager.Instance.SetDummyCameraActive(true);
+    }
+    #endregion
+
+    #region FadeOut
+    [SerializeField] private AnimationClip _fadeOutAnimation;
+
+    public void PlayFadeOut() => PlayClip(_fadeOutAnimation);
+
+    public void OnFadeOutStart()
+    {
+        Debug.LogWarning("On fade out start");
+        UIManager.Instance.SetDummyCameraActive(false);
+    }
+
+    public void OnFadeOutComplete()
+    {
+        Debug.LogWarning("On fade out complete");
+    }
+    #endregion
+
+    private void PlayClip(AnimationClip clip)
+    {
+        _mainMenuAnimator.Stop();
+        _mainMenuAnimator.clip = clip;
+        _mainMenuAnimator.Play();
+    }
+}
---
+++ UnityProjects/Game managers, Loaders, and the Game Loop/Assets/3. Game Manager/Scripts/Menu/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class UIManager : Singleton<UIManager>
+{
+    [SerializeField] private MainMenu _mainMenu;
+    [SerializeField] private Camera _dummyCamera;
+
+    public void SetDummyCameraActive(bool active)
+    {
+        _dummyCamera.gameObject.SetActive(active);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _mainMenu.PlayFadeOut();
+        }
+    }
+}
