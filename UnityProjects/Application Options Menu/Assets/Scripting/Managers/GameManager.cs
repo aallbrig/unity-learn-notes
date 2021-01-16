245b6c1ef7d4ce16bbe08909a1a02905f8d5a703
Andrew Allbright <aallbrig@gmail.com>
2021-01-16 07:39:49 -0500

    Game UI Challenge Project Start

---
+++ UnityProjects/Application Options Menu/Assets/Scripting/Managers/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    // Load Other Game Systems
+    // Unload
+    // Start Game
+    // Game Over
+}
---
+++ UnityProjects/Application Options Menu/Assets/Scripting/Utils/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = (T) this;
+        }
+        else
+        {
+            Debug.LogError("[Singleton] Instantiating multiple of what is suppose to be a singleton (bad)");
+        }
+    }
+
+    protected void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+}
