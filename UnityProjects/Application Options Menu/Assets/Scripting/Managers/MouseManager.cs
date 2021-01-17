4a6ac13f9efd179b39885ebf09672dab0e87fcad
Andrew Allbright <aallbrig@gmail.com>
2021-01-16 22:22:57 -0500

    Once in main scene, the player can be moved around.

--- UnityProjects/Application Options Menu/Assets/Scripting/Managers/MouseManager.cs
+++ UnityProjects/Application Options Menu/Assets/Scripting/Managers/MouseManager.cs
@@ -1,4 +1,33 @@
-﻿public class MouseManager : Singleton<MouseManager>
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
+
+public class MouseManager : Singleton<MouseManager>
 {
-    // Raycast each frame and set the cursor appropriately
+    public EventVector3 OnClickEnvironment;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        // Player right clicks
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out var hit, 50))
+            {
+                Debug.LogWarning(hit);
+                OnClickEnvironment?.Invoke(hit.point);
+            }
+            else
+            {
+                Debug.LogWarning("no hit");
+            }
+        }
+    }
 }
---
+++ UnityProjects/Application Options Menu/Assets/Scripting/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    public void SetDestination(Vector3 destination)
+    {
+        var cmd = new Move(_agent, destination);
+        cmd.Execute();
+    }
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        // Set animator to the current agent velocity
+    }
+}
